ffddda18535b101e6410afe9ed7ffda65e4d28ff
Schmelzy <schmelzyy@gmail.com>
2024-01-07 13:17:14 +0200

    Added new Book and Library controllers

--- BookStore.DL/MemoryDB/InMemoryDB.cs
+++ BookStore.DL/MemoryDB/InMemoryDB.cs
@@ -9,6 +9,23 @@ namespace BookStore.DL.MemoryDB
 {
     public static class InMemoryDB
     {
+        public static List<Book> BookData = new List<Book>()
+        {
+            new Book()
+            {
+                Id = 1,
+                AuthorID = 1,
+                ReleaseDate = new DateTime(2005,02,12),
+                Title = "Book 1"
+            },
+            new Book()
+            {
+                Id = 2,
+                AuthorID = 2,
+                ReleaseDate = new DateTime(2007,02,12),
+                Title = "Book 2"
+            }
+        };
         public static List<Author> AuthorData = new List<Author>()
         {
             new Author()
@@ -29,8 +46,6 @@ namespace BookStore.DL.MemoryDB
                 Name = "Author 3",
                 BirthDay = DateTime.Now
             }
-
-
         };
     }
 }
